d03d6216eec897473680329dc4e07c09f98e86fe
Tomasz Grochowski <tomasz.grochowski@bitbag.pl>
2021-05-11 14:34:21 +0200

    Update Sylius to version 1.9

--- src/DependencyInjection/BitBagSyliusMailChimpExtension.php
+++ src/DependencyInjection/BitBagSyliusMailChimpExtension.php
@@ -22,7 +22,7 @@ final class BitBagSyliusMailChimpExtension extends Extension
     /**
      * {@inheritdoc}
      */
-    public function load(array $config, ContainerBuilder $container)
+    public function load(array $config, ContainerBuilder $container): void
     {
         $loader = new YamlFileLoader($container, new FileLocator(__DIR__ . '/../Resources/config'));
         $loader->load('services.yml');
