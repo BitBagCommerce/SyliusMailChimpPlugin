8df849b74d7d33efe8e2c911485fff1d2f514cef
Tomasz Grochowski <tomasz.grochowski@bitbag.pl>
2021-07-12 23:05:10 +0200

    Update to sylius 1.10

--- tests/Application/config/sylius/1.8/bundles.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-declare(strict_types=1);
-
-return [
-    Doctrine\Bundle\DoctrineCacheBundle\DoctrineCacheBundle::class => ['all' => true],
-    WhiteOctober\PagerfantaBundle\WhiteOctoberPagerfantaBundle::class => ['all' => true],
-];
