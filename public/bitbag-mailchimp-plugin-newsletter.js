34d936699800a9375d270a8664d1f71872742f69
Marcin Szczepański <marcin.szczepanski@bitbag.pl>
2025-01-20 12:53:47 +0100

    OP-557 - upgrade to Sylius 2.0 - subscribe button js and bootstrap

--- public/bitbag-mailchimp-plugin-newsletter.js
+++ public/bitbag-mailchimp-plugin-newsletter.js
@@ -1,51 +1,48 @@
-(function () {
-    'use strict';
+window.joinNewsletter = function joinNewsletter(form) {
+    form.addEventListener('submit', function (event) {
+        event.preventDefault();
+        var successElement = form.querySelector('.success-element');
+        var input = form.querySelector('input[type=text]');
+        var validationElement = form.querySelector('.validation-element');
 
-    // Create a method to handle the newsletter form
-    function joinNewsletter(form) {
-        form.addEventListener('submit', function (event) {
-            event.preventDefault();
+        successElement.textContent = '';
+        validationElement.textContent = '';
 
-            var successElement = form.querySelector('.success-element');
-            var input = form.querySelector('input[type=text]');
-            var validationElement = form.querySelector('.validation-element');
+        var formData = new FormData(form);
+        var actionUrl = form.getAttribute('action');
+        var method = form.getAttribute('method');
 
-            successElement.textContent = '';
-            validationElement.textContent = '';
+        fetch(actionUrl, {
+            method: method,
+            body: formData
+        })
+            .then(async (response) => {
+                if (!response.ok) {
+                    response = await response.json();
+                    throw(response);
+                }
 
-            var formData = new FormData(form);
-            var actionUrl = form.getAttribute('action');
-            var method = form.getAttribute('method');
-
-            fetch(actionUrl, {
-                method: method,
-                body: formData
+                return await response.json()
             })
-                .then(response => response.json())
-                .then(data => {
-                    if (data.hasOwnProperty('message')) {
-                        successElement.innerHTML = data.message;
-                        input.value = '';
-                    }
-                })
-                .catch(error => {
-                    var message = 'An unexpected error occurred. Please try again later.';
-                    if (error.responseJSON && error.responseJSON.hasOwnProperty('errors')) {
-                        var errors = error.responseJSON.errors;
-                        message = '';
-                        for (var key in errors) {
-                            if (errors.hasOwnProperty(key)) {
-                                message += errors[key] + " ";
-                            }
-                        }
-                    }
-                    validationElement.textContent = message;
-                });
-        });
-    }
+            .then((data) => {
+                if (data.hasOwnProperty('message')) {
+                    successElement.innerHTML = data.message;
+                    input.value = '';
+                }
+            })
+            .catch(error => {
+                var message = 'An unexpected error occurred. Please try again later.';
 
-    // Applying the method to all forms with the .join-newsletter class
-    document.querySelectorAll('.join-newsletter').forEach(function (form) {
-        joinNewsletter(form);
+                if (error.errors) {
+                    try {
+                        let jsonErrors = JSON.parse(error.errors);
+                        message = '';
+                        message = Object.values(jsonErrors).join(" ");
+                    } catch (e) {}
+                }
+
+                validationElement.textContent = message;
+            });
     });
-})();
+}
+
